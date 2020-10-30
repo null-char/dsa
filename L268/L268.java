f70114032fc9ce7bfa767bacefdd99d49a63d386
null-char <skp.frl@gmail.com>
2020-10-30 22:37:53 +0530

    add L268

---
+++ L268/L268.java
@@ -0,0 +1,29 @@
+// Link: https://leetcode.com/problems/missing-number/
+package L268;
+
+class Solution {
+  public int missingNumber(int[] nums) {
+    int n = nums.length;
+
+    return ((n * (n + 1)) / 2) - arraySum(nums);
+  }
+
+  private int arraySum(int[] nums) {
+    int sum = 0;
+    for (int n : nums) {
+      sum += n;
+    }
+
+    return sum;
+  }
+}
+
+class Driver {
+  public static void main(String[] args) {
+    int[] nums = new int[] { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+    Solution soln = new Solution();
+
+    int res = soln.missingNumber(nums);
+    System.out.println(res);
+  }
+}
