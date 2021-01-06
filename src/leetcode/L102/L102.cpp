33ee6c86dc6cf8dc645564882055371380f9de11
null-char <skp.frl@gmail.com>
2021-01-06 21:43:55 +0530

    add L102

---
+++ src/leetcode/L102/L102.cpp
@@ -0,0 +1,49 @@
+// Link: https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+ public:
+  vector<vector<int>> levelOrder(TreeNode *root) {
+    vector<vector<int>> res;
+    if (root == NULL) return res;
+
+    int depth = -1;
+    queue<TreeNode *> q;
+    q.push(root);
+
+    while (!q.empty()) {
+      depth++;
+      int n = q.size();
+
+      for (int i = 0; i < n; i++) {
+        TreeNode *node = q.front();
+        q.pop();
+
+        if (depth >= res.size()) {
+          res.push_back({});
+        }
+
+        res[depth].push_back(node->val);
+        if (node->left) q.push(node->left);
+        if (node->right) q.push(node->right);
+      }
+    }
+
+    return res;
+  }
+};
+
+int main() {}
